d9d56e8904290be7089ecfe035f3324ce91f0b16
Tirtharaj Ghosh <tirtharajghosh.ju@gmail.com>
2020-10-24 14:49:35 +0530

    Added: Monotonic Array

---
+++ Arrays/Monotonic Array/MonotonicArray.java
@@ -0,0 +1,20 @@
+public class MonotonicArray {
+    public static boolean isMonotonic(int[] A) {
+        boolean isIncreasing = true;
+        boolean isDecreasing = true;
+        for(int i = 1; i<A.length; i++){
+            if(A[i-1]>A[i]){
+                isIncreasing = false;
+            }
+            
+            if(A[i-1]<A[i]){
+                isDecreasing = false;
+            }
+        }
+        return isIncreasing || isDecreasing;
+    }
+    public static void main(String[] args) {
+        int[] A = new int[]{8,6,5,4,4};
+        System.out.println(isMonotonic(A));
+    }
+}
