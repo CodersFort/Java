582023d017bd0c6fb4a36a76734e92a4fbbdfcc7
Tirtharaj Ghosh <58604532+tirtharajghosh@users.noreply.github.com>
2021-10-14 18:09:54 +0530

    Create Validate_BST.java

---
+++ Trees/Validate BST/Validate_BST.java
@@ -0,0 +1,23 @@
+class Solution {
+    // We use Integer instead of int as it supports a null value.
+    private Integer prev;
+
+    public boolean isValidBST(TreeNode root) {
+        prev = null;
+        return inorder(root);
+    }
+
+    private boolean inorder(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        if (!inorder(root.left)) {
+            return false;
+        }
+        if (prev != null && root.val <= prev) {
+            return false;
+        }
+        prev = root.val;
+        return inorder(root.right);
+    }
+}
